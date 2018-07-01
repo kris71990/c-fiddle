ecce849484a6de067ad7b8c0a5130e98b6b292b6
Kris Sakarias <ksakarias@gmail.com>
2018-07-01 12:57:06 -0700

    implementing bubble sort in c using pointers

---
+++ sorting/README.md
@@ -0,0 +1,13 @@
+# Sorting Algorithms in C
+
+## Bubble Sort
+
+Bubble Sort is a simple sorting algorithm to implement, although it is inefficient, especially for large data sets, compared to other sorting algorithms. 
+
+Although sorting in place in O(1) space, it requires two nested iterations of the array in order to compare and swap values. It runs in O(n^2) time.
+
+```
+To compile: `clang -o bubble-sort.out bubble-sort.c`
+
+To run: `./bubble-sort.out`
+```
---
+++ sorting/src/bubble-sort.c
@@ -0,0 +1,47 @@
+/* ---------
+Bubble Sort
+------------ */
+
+#include <stdio.h>
+
+// swap function
+// dereferencing pointer with *phigh, which gets the value of the int pointed to below
+void swap(int *phigh, int *plow) {
+  int temp = *phigh;
+  *phigh = *plow;
+  *plow = temp;
+}
+
+// pointers used when calling swap function below
+// &arr[y] points to the address in memory of the current int in question
+int main() {
+  int arr[] = { 
+    18, 77, 4, 666, 92, 33, 2, 42, 39, 14, 
+    60, 34, 66, 1, 919, 6, 9, 75, 99, 31, 55, 
+    16, 455, 7, 334, 235, 23, 19, 423, 16 
+  };
+
+  int size = sizeof(arr)/sizeof(arr[0]);
+
+  int x, y;
+  for (x = 0; x < size - 1; x++) {
+    for (y = 0; y < size - x - 1; y++) {
+      if (arr[y] > arr[y + 1]) {
+        swap(&arr[y], &arr[y + 1]);
+      }
+    }
+  }
+
+  printf("Sorted array:\n");
+
+  for (int z = 0; z < size; z++) {
+    if (z == 0) {
+      printf("[%d, ", arr[z]);
+    } else if (z == size - 1) {
+      printf("%d]\n", arr[z]);
+    } else {
+      printf("%d, ", arr[z]);
+    }
+  }
+  return 0;
+}
