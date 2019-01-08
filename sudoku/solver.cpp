e92444b9cd66538f384f22f010a9c88eae45eaf9
Kris Sakarias <ksakarias@gmail.com>
2019-01-08 15:56:00 -0800

    brute force sudoku solver in cpp

---
+++ sudoku/solver.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+
+#define GRID_FULL std::make_pair(9, 9)
+
+int count;
+
+int print_grid(int grid[][9]) {
+	for (int i = 0; i < 9; i++) {
+		std::cout << "\n";
+		std::cout << "-------------------------------------\n";
+		for (int j = 0; j < 9; j++) {
+			std::cout << " ";
+			if (grid[i][j] == 0) {
+				std::cout << " ";
+			}
+			else {
+				std::cout << grid[i][j];
+			}
+			std::cout << " |";
+		}
+	}
+	std::cout << "\n-------------------------------------\n\n";
+  return 0;
+}
+
+// check if valid in row
+bool exists_in_row(int grid[][9], const int& row, const int& num) {
+  for (int col = 0; col < 9; col++) {
+    if (grid[row][col] == num) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// check if valid in column
+bool exists_in_column(int grid[][9], const int& col, const int& num) {
+  for (int row = 0; row < 9; row++) {
+    if (grid[row][col] == num) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// check if valid in square
+bool exists_in_box(int grid[][9], const int& box_start_row, const int& box_start_col, const int& num) {
+  for (int row = 0; row < 3; row++) {
+    for (int col = 0; col < 3; col++) {
+      if (grid[row + box_start_row][col + box_start_col] == num) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// check if possible placement by validating with above functions
+bool validate_placement(int grid[][9], const int& row, const int& col, const int& num) {
+  bool in_row = exists_in_row(grid, row, num);
+  bool in_col = exists_in_column(grid, col, num);
+  bool in_box = exists_in_box(grid, row - row % 3, col - col % 3, num);
+  
+  return !in_row && !in_col && !in_box;
+}
+
+std::pair<int, int> get_unassigned_location(int grid[][9]) {
+	for (int row = 0; row < 9; row++)
+		for (int col = 0; col < 9; col++)
+			if (grid[row][col] == 0) {
+				return std::make_pair(row, col);
+			}
+	return GRID_FULL;
+}
+
+bool solve_soduko(int grid[][9]) {
+	if (GRID_FULL == get_unassigned_location(grid)) {
+		return true; 
+	}
+
+	std::pair<int, int> row_and_col = get_unassigned_location(grid);
+	int row = row_and_col.first;
+	int col = row_and_col.second;
+
+	for (int num = 1; num <= 9; num++) {
+		if (validate_placement(grid, row, col, num)) {
+			grid[row][col] = num;
+
+      count++;
+      std::cout << "Trying again - (" << count << " guesses)\n";
+
+			if (solve_soduko(grid)) {
+				return true;
+			}
+			grid[row][col] = 0;
+		}
+	}
+	return false; 
+}
+
+int main() {
+  std::cout << "\n************************************\n----------- Solve Sudoku -----------\n************************************\n";
+
+  int grid[9][9] = { 
+    { 0, 9, 0, 0, 0, 0, 8, 5, 3 },
+    { 0, 0, 0, 8, 0, 0, 0, 0, 4 },
+    { 0, 0, 8, 2, 0, 3, 0, 6, 9 },
+    { 5, 7, 4, 0, 0, 2, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 9, 0, 0, 6, 3, 7 },
+    { 9, 4, 0, 1, 0, 8, 5, 0, 0 },
+    { 7, 0, 0, 0, 0, 6, 0, 0, 0 },
+    { 6, 8, 2, 0, 0, 0, 0, 9, 0 },
+  };
+
+  print_grid(grid);
+
+  if (solve_soduko(grid) == true) {
+    print_grid(grid);
+  } else {
+    std::cout << "No solution exists.\n\n";
+  }
+  
+  return 0;
+}
