ce7d17d67234bca4f5c6eddc999624283531f4bc
Kris Sakarias <ksakarias@gmail.com>
2020-05-14 14:26:51 -0700

    add queen move function

--- chess/src/game.cpp
+++ chess/src/game.cpp
@@ -135,13 +135,66 @@ std::vector<std::array<int, 2>> rook_moves(const std::array<std::array<std::stri
   return moves;
 }
 
+std::vector<std::array<int, 2>> queen_moves(const std::array<std::array<std::string, 8>, 8>& board, int turn, int x, int y) 
+{
+  std::vector<std::array<int, 2>> moves;
+
+  bool piece_disruption1 = false;
+  bool piece_disruption2 = false;
+  bool piece_disruption3 = false;
+  bool piece_disruption4 = false;
+  bool piece_disruption5 = false;
+  bool piece_disruption6 = false;
+  bool piece_disruption7 = false;
+  bool piece_disruption8 = false;
+
+  for (int i = 1; i < 8; ++i) {
+    if (is_on_board(x + i, y) && !piece_disruption1) {
+      if (!is_unoccupied(board, x + i, y)) piece_disruption1 = true;
+      if (!piece_disruption1) moves.push_back({ x + i, y });
+    }
+    if (is_on_board(x - i, y)) {
+      if (!is_unoccupied(board, x - i, y)) piece_disruption2 = true;
+      if (!piece_disruption2) moves.push_back({ x - i, y });
+    }
+    if (is_on_board(x, y + i)) {
+      if (!is_unoccupied(board, x, y + i)) piece_disruption3 = true;
+      if (!piece_disruption3) moves.push_back({ x, y + i });
+    }
+    if (is_on_board(x, y - i)) {
+      if (!is_unoccupied(board, x, y - i)) piece_disruption4 = true;
+      if (!piece_disruption4) moves.push_back({ x, y - i });
+    }
+  }
+
+  for (int i = 1; i < 8; ++i) {
+    if (is_on_board(x + i, y + i) && !piece_disruption1) {
+      if (!is_unoccupied(board, x + i, y + i)) piece_disruption5 = true;
+      if (!piece_disruption5) moves.push_back({ x + i, y + i });
+    }
+    if (is_on_board(x - i, y - i)) {
+      if (!is_unoccupied(board, x - i, y - i)) piece_disruption6 = true;
+      if (!piece_disruption6) moves.push_back({ x - i, y - i });
+    }
+    if (is_on_board(x - i, y + i)) {
+      if (!is_unoccupied(board, x - i, y + i)) piece_disruption7 = true;
+      if (!piece_disruption7) moves.push_back({ x - i, y + i });
+    }
+    if (is_on_board(x + i, y - i)) {
+      if (!is_unoccupied(board, x + i, y - i)) piece_disruption8 = true;
+      if (!piece_disruption8) moves.push_back({ x + i, y - i });
+    }
+  }
+  return moves;
+}
+
 std::vector<std::array<int, 2>> knight_moves(const std::array<std::array<std::string, 8>, 8>& board, int turn, int x, int y) 
 {
   std::vector<std::array<int, 2>> moves;
 
-  // for (int i = 0; i < 8; ++i) {
+  for (int i = 0; i < 8; ++i) {
 
-  // }
+  }
 
   return moves;
 }
@@ -199,6 +252,14 @@ std::string is_valid_move(Board& board, int turn, int xFrom, int yFrom, int xTo,
             return piece_type;
           }
         } 
+      } else if (piece_type == "Q") {
+        possible_moves = queen_moves(board.board, turn, it_from -> first.x, it_from -> first.y);
+        board.print_possible_moves(possible_moves);
+        for (std::array<int, 2>& move : possible_moves) {
+          if (move[0] == xTo && move[1] == yTo) {
+            return piece_type;
+          }
+        } 
       }
     } else {
       return "";
@@ -243,6 +304,14 @@ std::string is_valid_move(Board& board, int turn, int xFrom, int yFrom, int xTo,
         } 
       } else if (piece_type == "R") {
         possible_moves = rook_moves(board.board, turn, it_from -> first.x, it_from -> first.y);
+        board.print_possible_moves(possible_moves);
+        for (std::array<int, 2>& move : possible_moves) {
+          if (move[0] == xTo && move[1] == yTo) {
+            return piece_type;
+          }
+        } 
+      } else if (piece_type == "Q") {
+        possible_moves = queen_moves(board.board, turn, it_from -> first.x, it_from -> first.y);
         board.print_possible_moves(possible_moves);
         for (std::array<int, 2>& move : possible_moves) {
           if (move[0] == xTo && move[1] == yTo) {
